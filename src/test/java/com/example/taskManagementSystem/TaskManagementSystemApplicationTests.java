2ae19d04ec1d02d8e477554413423d2054a8ef70
Maxim <therikomero@gmail.com>
2024-12-27 04:51:27 +0700

    cleanup

--- src/test/java/com/example/taskManagementSystem/TaskManagementSystemApplicationTests.java
+++ src/test/java/com/example/taskManagementSystem/TaskManagementSystemApplicationTests.java
@@ -5,15 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TaskManagementSystemApplicationTests {
-
     @Test
     void contextLoads() {
-
     }
-
-    @Test
-    void testUserAccessToTask() {
-
-    }
-
 }
