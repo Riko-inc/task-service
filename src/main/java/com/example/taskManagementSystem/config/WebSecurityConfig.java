e30ed2858e6e8b22192c21819cd267af3830fef5
Maxim <therikomero@gmail.com>
2025-04-06 22:11:29 +0700

    refactor old auth module in task

--- src/main/java/com/example/taskManagementSystem/config/WebSecurityConfig.java
+++ src/main/java/com/example/taskManagementSystem/config/WebSecurityConfig.java
@@ -43,7 +43,6 @@ public class WebSecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthFilter;
     private final AuthenticationProvider authenticationProvider;
-    private final LogoutHandler logoutHandler;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -59,7 +58,6 @@ public class WebSecurityConfig {
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .logout(logout -> logout.logoutUrl("/api/v1/auth/logout")
-                                .addLogoutHandler(logoutHandler)
                                 .logoutSuccessHandler((request, response, authentication) ->
                                         SecurityContextHolder.clearContext()));
         return http.build();
