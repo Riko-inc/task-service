88875ad7dcb633c1dea1b2677a9e711a0ae1d5dd
Maxim <therikomero@gmail.com>
2025-04-20 22:24:35 +0700

    even better validation

--- src/main/java/com/example/taskManagementSystem/exceptions/ExceptionResponseHandler.java
+++ src/main/java/com/example/taskManagementSystem/exceptions/ExceptionResponseHandler.java
@@ -3,12 +3,13 @@ package com.example.taskManagementSystem.exceptions;
 import com.example.taskManagementSystem.domain.dto.responses.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -29,13 +30,26 @@ public class ExceptionResponseHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
-        Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getFieldErrors().forEach(error -> {
-            String fieldName = error.getField();
-            String errorMessage = error.getDefaultMessage();
-            errors.put(fieldName, errorMessage);
-        });
-        return ResponseEntity.badRequest().body(errors);
+    public ErrorResponse handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, Object> fieldErrors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                        fieldErrors.put(error.getField(), error.getDefaultMessage())
+                );
+
+        return ErrorResponse.builder()
+                .id(UUID.randomUUID())
+                .message("Request fields validation failed")
+                .params(fieldErrors)
+                .build();
+    }
+
+    @ExceptionHandler({HttpMessageNotReadableException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleHttpMessageNotReadable(HttpMessageNotReadableException ex) {
+        return ErrorResponse.builder()
+                .id(UUID.randomUUID())
+                .message(ex.getMostSpecificCause().getMessage())
+                .params(Collections.emptyMap())
+                .build();
     }
 }
