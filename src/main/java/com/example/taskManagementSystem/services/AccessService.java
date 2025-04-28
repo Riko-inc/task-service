bfaa6cc0d5ef6594f21625dacb3ddd5abd512097
Maxim <therikomero@gmail.com>
2025-04-29 00:42:37 +0700

    add kafka configuration

--- src/main/java/com/example/taskManagementSystem/services/AccessService.java
+++ src/main/java/com/example/taskManagementSystem/services/AccessService.java
@@ -11,7 +11,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.Objects;
-import java.util.Optional;
 
 /**
  * Defines assess rules for users needed to perform actions
